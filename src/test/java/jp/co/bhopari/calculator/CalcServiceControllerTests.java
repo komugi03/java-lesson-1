7f8daabce7031de7b9eedbc64c23554454e27107
Natsumi Matsuzawa <matsuzawa-nm@bhopari.onmicrosoft.com>
2020-08-19 16:02:52 +0900

    add MockMvc

--- src/test/java/jp/co/bhopari/calculator/CalcServiceControllerTests.java
+++ src/test/java/jp/co/bhopari/calculator/CalcServiceControllerTests.java
@@ -1,5 +1,9 @@
 package jp.co.bhopari.calculator;
 
+/**
+ * コントローラの単体テスト
+ */
+
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -15,7 +19,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import jp.co.bhopari.calculator.controllers.CalcServiceController;
 import jp.co.bhopari.calculator.services.CalcService;
 
-//コントローラのテスト
 @SpringBootTest
 public class CalcServiceControllerTests {
 
@@ -27,24 +30,24 @@ public class CalcServiceControllerTests {
 
 	//モック化したサービスを作る
 	@MockBean
-	CalcService calc;	//CalcServiceはインターフェース
+	CalcService calc;
 
-	@BeforeEach		//それぞれのテストの前に実行されるもの
+	//それぞれのテストの前に実行されるもの
+	@BeforeEach
 	public void setup() {
 		mockMvc = MockMvcBuilders
-					.standaloneSetup(cont)
-					.build();
+				.standaloneSetup(cont)
+				.build();
 	}
 
 	@Test
 	public void test() throws Exception {
 
-		//テスト1　モデルの中身が123か確認↓
 
-		//モックアップを設定する。テストサービスのaddが呼び出されたときに、123か確認したい。
+		//モックアップを設定する。
 		//加算
 		when(calc.add(100,23))
-			.thenReturn(123.0);
+		.thenReturn(123.0);
 
 		//減算
 		when(calc.subtract(100,23))
@@ -58,18 +61,19 @@ public class CalcServiceControllerTests {
 		when(calc.divide(100,23))
 		.thenReturn(123.0);
 
+		//テストサービスのaddが呼び出されたときに123か確認
 		//リクエストの発行(今回はWebからのリクエスト)
-		mockMvc.perform(get("/calc"))
-				.andExpect(model().attribute("answer",123.0))
-												//変数名, 期待値
-				.andExpect(view().name("CalcService"))	//Web特有 指定のview(CalcService.html)を返すか？
-				.andExpect(status().isOk());	//Web特有 HTTPステータスコードのテストなのでstatus()を使う
+		mockMvc.perform(get("/calc").param("inputX", "100").param("inputY", "23").param("operator", "ADD").param("do", "計算"))
+		.andExpect(model().attribute("answer", "123.0"))
 
-// 		//テスト2　TestService#getRandomNumber()が呼ばれていることを確認↓
-//		verify(calc, times(1)).add();
-//		//↑で、testServiceというモックの.getRandomNumber()が一回返ってきたことを確認できる
+		//Web特有 指定のview(CalcService.html)を返すか？
+		.andExpect(view().name("calcservice"))
+
+		//Web特有 HTTPステータスコードのテストなのでstatus()を使う
+		.andExpect(status().isOk());
+
+		//addメソッドが一度呼び出されたことを確認
+		verify(calc, times(1)).add(100, 23);
 
 	}
-
-
 }
